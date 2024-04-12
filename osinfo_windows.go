6340ba05fb97f67b43f71947bd728fe452fc3c10
Eliseo Martelli <me@eliseomartelli.it>
2024-04-12 09:41:19 +0200

    feat: add windows implementation

--- osinfo_windows.go
+++ osinfo_windows.go
@@ -2,9 +2,19 @@
 
 package goosinfo
 
+import "strings"
+
 func (i *OSInfo) retrieve() error {
 	i.Distribution = "Windows"
-
-	// i.Version =
+	path, err := commander.LookPath("cmd")
+	if err != nil {
+		return err
+	}
+	cmd := commander.Command(path, "/C", "ver")
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	i.Version = strings.TrimSpace(string(out))
 	return nil
 }
