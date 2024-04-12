f56287de4406ee977385080094917b08bc9cfd75
Eliseo Martelli <me@eliseomartelli.it>
2024-04-12 09:41:05 +0200

    feat: add macos implementation

--- osinfo_macos.go
+++ osinfo_macos.go
@@ -2,8 +2,19 @@
 
 package goosinfo
 
+import "strings"
+
 func (i *OSInfo) retrieve() error {
 	i.Distribution = "macOS"
-	// i.Version =
+	path, err := commander.LookPath("sw_vers")
+	if err != nil {
+		return err
+	}
+	cmd := commander.Command(path, "-productVersion")
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	i.Version = strings.TrimSpace(string(out))
 	return nil
 }
