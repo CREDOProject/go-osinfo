5c68537f3a3623546f725edd54fc05fd5f5ac3b0
Eliseo Martelli <me@eliseomartelli.it>
2024-04-12 09:40:50 +0200

    feat: add linux implementation

--- osinfo_linux.go
+++ osinfo_linux.go
@@ -2,7 +2,28 @@
 
 package goosinfo
 
+import (
+	"os"
+	"strings"
+)
+
 func (i *OSInfo) retrieve() error {
+	file, err := os.ReadFile("/etc/os-release")
+	if err != nil {
+		return err
+	}
+	fileString := strings.Split(string(file), "\n")
+	var kv = make(map[string]string)
+	for _, line := range fileString {
+		splits := strings.Split(line, "=")
+		if len(splits) > 1 {
+			key := splits[0]
+			val := strings.Trim(splits[1], "\"")
+			kv[key] = val
+		}
+	}
+	i.Distribution = kv["ID"]
+	i.Version = kv["VERSION_ID"]
 
 	return nil
 }
